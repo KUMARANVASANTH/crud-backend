f5e3d4fca2f0b64ef4b2b3e8df62a2b4c5a4626d
VASANTHARAN <107398944+KUMARANVASANTH@users.noreply.github.com>
2023-05-26 09:34:46 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 module.exports.connectdb = () => {
-    return mongoose.connect('mongodb://127.0.0.1:27017/books');
+    return mongoose.connect('mongodb+srv://vasanth:vasanth@testing.tngqamu.mongodb.net/books');
 };
